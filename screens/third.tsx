e2c158fe0bb1a141a8f270993ae74ebfc89fc625
Eduardo <cesconettoedu@gmail.com>
2024-10-17 23:06:49 -0400

    clean

--- screens/third.tsx
+++ screens/third.tsx
@@ -1,12 +1,21 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet  } from 'react-native'
 import React from 'react'
 
 const Third = () => {
   return (
-    <View style={{ flex: 1, backgroundColor: 'lightgray', justifyContent: 'center', alignItems: 'center' }}>
+    <View style={styles.container}>
       <Text>Third Test</Text>
     </View>
   )
 }
 
 export default Third
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, 
+    backgroundColor: 'lightblue', 
+    justifyContent: 'center', 
+    alignItems: 'center'
+  }
+})
--- screens/two.tsx
+++ screens/two.tsx
@@ -1,12 +1,21 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet  } from 'react-native'
 import React from 'react'
 
 const Two = () => {
   return (
-    <View style={{ flex: 1, backgroundColor: 'lightgreen', justifyContent: 'center', alignItems: 'center' }}>
+    <View style={styles.container}>
       <Text>Two Test</Text>
     </View>
   )
 }
 
 export default Two
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, 
+    backgroundColor: 'lightblue', 
+    justifyContent: 'center', 
+    alignItems: 'center'
+  }
+})
